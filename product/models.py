4c3964e7f9e87b0d8d8821fb6970370aff0a4638
Islam412 <islam.124.hamdy@gmail.com>
2023-08-25 20:54:48 +0300

    add translation to product models

--- product/models.py
+++ product/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
 FLAG_TYPES=[
@@ -9,28 +11,29 @@ FLAG_TYPES=[
     ('Feature','Feature'),
 ]
 class Product(models.Model):
-    name = models.CharField(max_length=120)
-    flag = models.CharField(max_length=10,choices=FLAG_TYPES)
-    image = models.ImageField(upload_to='products')
-    price = models.FloatField()
-    sku = models.CharField(max_length=12)
-    subtitle = models.CharField(max_length=300)
-    descripition = models.TextField(max_length=40000)
-    quantity = models.IntegerField()
-    brand = models.ForeignKey('Brand', related_name='product_name', on_delete=models.SET_NULL, null = True)
+    name = models.CharField(_('Name'),max_length=120)
+    flag = models.CharField(_('Flag'),max_length=10,choices=FLAG_TYPES)
+    image = models.ImageField(_('Image'),upload_to='products')
+    price = models.FloatField(_('Price'))
+    sku = models.CharField(_('SKU'),max_length=12)
+    subtitle = models.CharField(_('Subtitle'),max_length=300)
+    descripition = models.TextField(_('Descripition'),max_length=40000)
+    quantity = models.IntegerField(_('Quantity'))
+    brand = models.ForeignKey('Brand',verbose_name=_('Brand'), related_name='product_name', on_delete=models.SET_NULL, null = True)
 
 
 
 
 class ProductImage(models.Model):
-    pass
+    product = models.ForeignKey(Product ,verbose_name=_('Product'), related_name='product_image', on_delete=models.CASCADE)
+    image = models.ImageField(_('Image'),upload_to='product_images')
 
 
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='brands')
+    name = models.CharField(_('Name'),max_length=100)
+    image = models.ImageField(_('Image'),upload_to='brands')
 
     def __str__(self):
         return self.name
@@ -39,8 +42,8 @@ class Brand(models.Model):
 
 
 class Review(models.Model):
-    user = models.ForeignKey(User,related_name='review_author',on_delete=models.SET_NULL,null=True)
-    product = models.ForeignKey(Product, related_name='review_product',on_delete=models.CASCADE)
-    rate = models.IntegerField()
-    revuiew = models.CharField(max_length=300)
-    created_at = models.DateTimeField(default=timezone.now())
+    user = models.ForeignKey(User,verbose_name=_('User'),related_name='review_author',on_delete=models.SET_NULL,null=True)
+    product = models.ForeignKey(Product,verbose_name=_('Product'), related_name='review_product',on_delete=models.CASCADE)
+    rate = models.IntegerField(_('Rate'))
+    revuiew = models.CharField(_('Revuiew'),max_length=300)
+    created_at = models.DateTimeField(_('Created_at'),default=timezone.now())
