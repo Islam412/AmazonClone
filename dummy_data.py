c352e2b8de8f26060963c60e3efc8a199432aafe
Islam412 <islam.124.hamdy@gmail.com>
2023-09-01 18:18:23 +0300

    add dummy data for products

--- dummy_data.py
+++ dummy_data.py
@@ -14,7 +14,7 @@ def seed_brand(n):
     for _ in range(n):
         Brand.objects.create(
             name=fake.name() ,
-            image = f'brands/{image[random.randint(0,44)]}'
+            image = f'brands/{image[random.randint(0,40)]}' ,
         )
     print(f'Seed {n} Brands Successfully')
 
@@ -22,14 +22,25 @@ def seed_brand(n):
 
 
 def seed_product(n):
-    pass
+    fake = Faker()
+    image = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpeg','11.jpeg','12.jpeg','13.jpeg','14.jpeg','15.jpeg','16.jpeg','17.jpg','18.jpg','19.jpg','20.jpg','21.png','22.jpg','23.jpg','24.jpg','25.jpg','26.jpg','27.jpg','28.jpg','29.jpg','30.jpg','31.jpg','32.jpg','33.jpg','34.jpeg','35.jpg','36.jpg','37.png','38.png','39.jpg','40.jpg','41.png','42.jpg','43.png']
+    flag = ['Sale','New','Feature']
+    for _ in range(n):
+        Product.objects.create(
+            name=fake.name() ,
+            image = f'products/{image[random.randint(0,40)]}' ,
+            flag = flag[random.randint(0,2)] ,
+            price = round(random.uniform(20.99,99.99),2) ,
+            sku = random.randint(1000,10000000) ,
+            subtitle = fake.text(max_nb_chars=250) ,
+            descripition = fake.text(max_nb_chars=20000) ,
+            quantity = random.randint(0,30) ,
+            brand = Brand.objects.get(id=random.randint(1,280)) ,
+                    )
+    print(f'Seed {n} Products Successfully')
 
 
 
 
-
-
-
-
-seed_brand(5)
-#seed_product(5)
+#seed_brand(100)
+seed_product(2000)
