bb22b6782e936ecd6b546da093298d634206756a
Islam412 <islam.124.hamdy@gmail.com>
2023-09-04 02:41:33 +0300

    know work sqil in background

--- product/views.py
+++ product/views.py
@@ -1,10 +1,91 @@
 from django.shortcuts import render
 from django.views.generic import ListView , DetailView
 from .models import Product, ProductImage, Brand, Review
+from django.db.models import Q , F
+from django.db.models.aggregates import Max,Min,Count,Avg,Sum
 
 def queryset_depug(request):
-    data = Product.objects.all()
-    return render(request,'product/debug.html',{'data':data})
+    #data = Product.objects.select_related('brand').all() #prefetch_related = many-to-many 
+    #filter
+    #data = Product.objects.filter(price__gt= 70) #<(__gt)
+    #data = Product.objects.filter(price__gte= 70) #<=(__gte)
+    #data = Product.objects.filter(price__lt= 70) #>(__lt)
+    #data = Product.objects.filter(price__lte= 70) #>=(__lte)
+    #data = Product.objects.filter(price__range = (65,70)) #(__range(x,y))
+    
+    
+    #navigate relation
+    #data = Product.objects.filter(brand__name='Connie Diaz')
+    #data = Product.objects.filter(brand__price__gt=20) #navigate relation with (gt,lt)
+    
+    #filter with text
+    #data = Product.objects.filter(name__contains='Johnson') #search for '....'
+    #data = Product.objects.filter(name__startswith='Danielle') #search for start
+    #data = Product.objects.filter(name__endswith='Novak') #search for end
+    
+    #filter tag
+    #data = Product.objects.filter(tags__isnull=True) #search for tag
+    
+    
+    #filter date & time
+    #data = Review.objects.filter(created_at__year=2023) #search for date & time
+    #data = Review.objects.filter(created_at__month=2023) #search for date & time
+    
+    
+    #filter 2 values
+    #data = Product.objects.filter(price__gt=80,quantity__lt=10) #search 2 values(and)
+    '''data = Product.objects.filter(
+        Q(quantity__lt=10)|
+        Q(price__gt=80)
+    ) #search 2 values(Q   or)'''
+
+    '''data = Product.objects.filter(
+        Q(quantity__lt=10)&
+        Q(price__gt=80)
+    ) #search 2 values(Q   &)'''
+
+    ''' data = Product.objects.filter(
+        Q(quantity__lt=10)|
+        ~Q(price__gt=80)
+    ) #search 2 values(Q   not)~~~~~~~~'''
+
+    #data = Product.objects.filter(price=F('quantity')) #search tower == tower (f)
+    
+    
+    #data = Product.objects.all().order_by('name')#ASC
+    #data = Product.objects.order_by('name')#ASC
+    #data = Product.objects.order_by('name','quantity')#ASC
+    #data = Product.objects.order_by('name').reverse#DES
+    
+    #erorr
+    #data = Product.objects.order_by('name')[0]#first
+    #data = Product.objects.order_by('name')[-1]#last
+
+
+
+    #data = Product.objects.earliest('name')#first
+    #data = Product.objects.latest('name')#last
+    
+    
+    #slice
+    #data = Product.objects.all()[:10]
+    #data = Product.objects.all()[10:20]
+    
+    #select columns
+    #data = Product.objects.values('name','price')
+    #data = Product.objects.values('name','price','brand__name')#dech
+    #data = Product.objects.values_list('name','price','brand__name')#list
+    
+    
+    #remove dublicate
+    #data = Product.objects.all().distinct()
+
+    #data = Product.objects.only('name','price')
+    #data = Product.objects.defer(')
+    
+    #aggregate
+    data = Product.objects.aggregate(Sum('quantity'))
+    return render(request,'product/debug.html',{'data':data}) 
 
 
 class ProductList(ListView):
