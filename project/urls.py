55f04fb41433f552812780386c02fe63f0cdb634
Islam412 <islam.124.hamdy@gmail.com>
2023-09-08 20:38:16 +0300

    add brands and sale_broducts in home padge

--- project/urls.py
+++ project/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/', include('product.urls')),
+    path('', include('settings.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
     ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.db.models import Count
+from product.models import Product , Brand , Review
 # Create your views here.
+
+
+
+def home(request):
+    brands = Brand.objects.all().annotate(product_count=Count('product_name'))
+    sale_broducts = Product.objects.filter(flag='Sale')[:10]
+    Feature_broducts = Product.objects.filter(flag='Feature')[:6]
+    new_broducts = Product.objects.filter(flag='New')[:10]
+    reviews = Review.objects.all()[:5]
+    return render(request,'settings/home.html',{
+        'brands':brands ,
+        'sale_broducts':sale_broducts ,
+        'Feature_broducts':Feature_broducts ,
+        'new_broducts':new_broducts ,
+        'reviews':reviews ,
+    })
