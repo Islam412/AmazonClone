45cfdf0c12f46e11b703661f7b2acbb58613d410
Islam412 <islam.124.hamdy@gmail.com>
2023-09-08 23:29:45 +0300

    add Featured Items , New Items

--- settings/views.py
+++ settings/views.py
@@ -8,13 +8,13 @@ from product.models import Product , Brand , Review
 def home(request):
     brands = Brand.objects.all().annotate(product_count=Count('product_name'))
     sale_broducts = Product.objects.filter(flag='Sale')[:10]
-    Feature_broducts = Product.objects.filter(flag='Feature')[:6]
+    feature_broducts = Product.objects.filter(flag='Feature')[:6]
     new_broducts = Product.objects.filter(flag='New')[:10]
     reviews = Review.objects.all()[:5]
     return render(request,'settings/home.html',{
         'brands':brands ,
         'sale_broducts':sale_broducts ,
-        'Feature_broducts':Feature_broducts ,
+        'feature_broducts':feature_broducts ,
         'new_broducts':new_broducts ,
         'reviews':reviews ,
     })
