5c425503af06b1807c173df5d299e147dcb2cf6d
Islam412 <islam.124.hamdy@gmail.com>
2023-09-29 02:37:46 +0300

    show cart total in base , add in models

--- orders/cart_context_processor.py
+++ orders/cart_context_processor.py
@@ -4,8 +4,8 @@ def get_or_create_cart(request):
     if request.user.is_authenticated:
         cart, created = Cart.objects.get_or_create(user=request.user, status='InProgress')
         if not created:
-            cart_detail_data = CartDetail.objects.filter(cart=cart)  # Renamed cart_detail to cart_detail_data
-            return {'cart_data': cart, 'cart_detail_data': cart_detail_data}  # Renamed cart_detail to cart_detail_data
+            cart_detail = CartDetail.objects.filter(cart=cart)  # Renamed cart_detail to cart_detail_data
+            return {'cart_data': cart, 'cart_detail_data': cart_detail}  # Renamed cart_detail to cart_detail_data
         return {'cart_data': cart}
     else:
         return {}
