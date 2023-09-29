c6f004fb0e21d9b4ddbfbd15e9b436078e0b499c
Islam412 <islam.124.hamdy@gmail.com>
2023-09-29 21:43:33 +0300

    show order with detail in orderlist page & add pagpaginate in orderlist page

--- orders/views.py
+++ orders/views.py
@@ -6,3 +6,8 @@ from .models import Order
 
 class OrderList(ListView):
     model = Order
+    paginate_by = 10
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(user=self.request.user)
+        return queryset
+    
