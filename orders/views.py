a57cbddde8f29b30925c0017ef222b4db4ab20a7
Islam412 <islam.124.hamdy@gmail.com>
2023-10-07 11:47:27 +0300

    add to cart

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Order , OrderDetail , Cart ,CartDetail
+from product.models import Product
 
 
 
@@ -15,8 +16,15 @@ class OrderList(LoginRequiredMixin, ListView):
     
 
 
-def add_to_cart(self):
-    pass
+def add_to_cart(request):
+    quantity = request.POST['quantity']
+    product = Product.objects.get(id=request.POST['product_id'])
+    cart = Cart.objects.get(user=request.user,status='InProgress')
+    cart_detail, created = CartDetail.objects.get_or_create(cart=cart, product=product)
+    cart_detail.quantity = int(quantity)
+    cart_detail.total = round(int(quantity) * product.price,2)
+    cart_detail.save()
+    return redirect(f'/product/{product.slug}')
 
 
 @login_required
