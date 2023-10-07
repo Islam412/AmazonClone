8469a22bc714bdb9db4b08e39b5cb9d10ea1b186
Islam412 <islam.124.hamdy@gmail.com>
2023-10-07 16:32:07 +0300

    add cart serializers

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,4 +3,15 @@ from .models import Cart,CartDetail,Order,OrderDetail
 
 
 
+class CartDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartDetail
+        fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    cart_detail = CartDetailSerializer(many=True)
+    class Meta:
+        model = Cart
+        fields = '__all__'
 
