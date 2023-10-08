14a6d9a738bb1974b17269e916a11d469cb8a580
Islam412 <islam.124.hamdy@gmail.com>
2023-10-08 23:27:25 +0300

    add order serializers

--- orders/serializers.py
+++ orders/serializers.py
@@ -18,3 +18,23 @@ class CartSerializer(serializers.ModelSerializer):
         model = Cart
         fields = '__all__'
 
+
+class OrderListSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class OrderProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderDetail
+        fields = '__all__'
+
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    products = OrderProductSerializer(many=True,source='order_detail')
+    class Meta:
+        model = Order
+        fields = '__all__'
